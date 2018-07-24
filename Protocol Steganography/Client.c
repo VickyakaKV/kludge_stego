d42eb251bc2f56a9d402de90fb4f62ee8aebf0dc
VickyakaKV <32714604+VickyakaKV@users.noreply.github.com>
2018-07-24 10:09:43 +0530

    Add files via upload

--- Protocol Steganography/Client.c
+++ Protocol Steganography/Client.c
@@ -175,24 +175,9 @@ int main (int argc, char const *argv[]){
         exit(0);
     }
 
-    struct sockaddr_in address;
-    int sock = 0, valread;
-    struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
-    char buff[1024] = {0};
+    int sock = 0;
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
         printf("\n Socket creation error \n");
-        return -1;
-    }
-  
-    memset(&serv_addr, '0', sizeof(serv_addr));
-  
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(44946);
-      
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
-        printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
 
